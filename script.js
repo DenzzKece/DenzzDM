34fe74001478b272f1817cfbd382276874b2b66b
DenzzKece <denisfirmansah30@gmail.com>
2025-10-28 11:26:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. pilih tombol berdasar id-nya
+const tombolSapa=document.getElementByld('sapaButton')
+//2.tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click',function() b{
+    alert('Halo! Terima Kasih sudah berkunjung~!');
+});
